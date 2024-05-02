ce2b745dbcdd771ac3135a0583d218a895ddc498
MatteoDiMartino <matteodimartino359@gmail.com>
2024-05-02 15:15:14 +0200

    feat: add base element with style

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    
+<div class="container">
+    <div class="row">
+        <div class="w-25 my-5 d-flex">
+            <div class="d-flex me-5">
+                <div class="p-4 ms-box-card bg-info justify-content-center align-items-center rounded-2">
+                    <div class="box-img rounded-top-3"></div>
+                    <div>
+                        <div class="box-txt bg-warning rounded-bottom-3 px-2 text-center">[text]</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="d-flex">
+                <div class="p-4 ms-box-card bg-info justify-content-center align-items-center rounded-2">
+                    <div class="box-img rounded-top-3"></div>
+                    <div>
+                        <div class="box-txt bg-warning rounded-bottom-3 px-2 text-center">[text]</div>
+                    </div>
+                </div>
+            </div>
+            
+
+        </div>
+    </div>
+</div>
+
+
+
+</body>
+</html>
